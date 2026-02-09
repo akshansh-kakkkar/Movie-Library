9c24f1a14f89b4d59dc8673dd1dc6c64037c883b
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-02-09 12:28:17 +0530

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css'
 import Movies from './Movies'
-
 function App() {
 
   return (
     <>
-    <h1>Hey I am AKshansh</h1>
     <Movies/>
     </>
   )
--- src/Movies.jsx
+++ src/Movies.jsx
@@ -3,25 +3,55 @@ import moviesData from "./Movies.json"
 const Movies = () => {
   const [loading, setLoading] = useState(false);
   const [genre, setGenre] = useState("All");
-  const [movies, setMovies] = useState(moviesData);
-  const [searchTerm, setSearchTerm] = useState("")
-    
+  const [movie, setMovies] = useState(moviesData.movies);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortby, setSortby] = useState("title")
+  const Rating = ()=>{
+    if(movie.rating >= 8){
+      return "Super-hit";
+    }
+    if(movie.rating>=7){
+      return "hit";
+    }
+    if(movie.rating>=6){
+      return "average"
+    }
+    return "flop"
+  }
   return (
-    <div>
-        {moviesData.movies.map((movies)=>(
-            <div key={movies.id}>
-                <h1>{movies.title}</h1>
-                <div><img src={movies.image} alt={movies.alt} style={{width:300}}/></div>
-                <div>{movies.rating}</div>
-                <div><h1>{movies.author}</h1></div>
-                <div><h1>{movies.genre}</h1></div>
-                <div><p>{movies.year}</p></div>
-                <div>{movies.director}</div>
-                <div>{movies.cast}</div> 
-                        
-            </div>
-        ))}
+    <>
+        <div className='flex justify-center text-white text-5xl font-bold  p-5'>
+      <h1 className='text-shadow-white text-shadow-2xl '>Movie Hits</h1>
     </div>
+    <div className='flex justify-center '>
+        {
+      loading  ? (
+        <div>
+          <p>Loading....</p>
+        </div>
+      ) : (
+        <div className='grid md:grid-cols-4'>
+          {movie.map(movies=>(
+            <div key={movies.id}>
+              <h1>{movies.title}</h1>
+              <div>
+                <img src={movies.image} alt={movies.alt} style={{width:300, height:400}}/>
+              </div>
+              <div>
+                <p>{movies.director}</p>
+                <p>{movies.writer}</p>
+                <p >{movies.cast.join(", ")}</p>
+                <p>{movies.genre}</p>
+                <p>{movies.year}</p>
+                <p >{movies.rating}/10</p>
+              </div>
+              </div>
+          ))}
+        </div>
+      )
+    }
+    </div>
+    </>
   )
 }
 
